927ae6e44d4c80b175a22307fe99471130ac8869
Marcel Kenlay <mkenlay16@gmail.com>
2018-12-05 17:47:33 +0000

    Added 362 project

--- src/Components/Files/assets.js
+++ src/Components/Files/assets.js
@@ -2,6 +2,7 @@ const assetsObject = {
   matchUpsImage: require('./MatchUps.jpg'),
   tron: require('./tron.png'),
   tournamentTracker: require('./TournamentTracker.JPG'),
-  personalPage: require('./PersonalPage.jpg')
+  personalPage: require('./PersonalPage.jpg'),
+  entityAspectSentimentAnalysis: require('./EntityApectSentimentAnalysis.JPG')
 }
 module.exports = assetsObject
